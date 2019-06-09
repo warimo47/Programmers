a0152c54b5a9a235deacbbf1440545a9c8bdd98c
warimo47 <warimo47@naver.com>
2019-06-09 13:21:27 +0900

    H-Index Complete

--- Programmers/H-Index.cpp
+++ Programmers/H-Index.cpp
@@ -1,14 +1,30 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations)
 {
-	int answer = 0;
+	int answer = citations.size();
+	int count = 0;
 
+	sort(citations.begin(), citations.end(), [](int a, int b) {return a > b; });
 
+	while(true)
+	{
+		if (count >= answer) break;
+
+		if (citations[count] >= answer)
+		{
+			count++;
+		}
+		else
+		{
+			answer--;
+		}
+	}
 
 	return answer;
 }
@@ -16,6 +32,22 @@ int solution(vector<int> citations)
 int main()
 {
 	vector<int> clothes = { 3, 0, 6, 1, 5 };
+
+	cout << solution(clothes) << "\n";
+
+	clothes = { 25, 8, 5, 3, 3 };
+
+	cout << solution(clothes) << "\n";
+
+	clothes = { 10, 8, 5, 4, 3 };
+
+	cout << solution(clothes) << "\n";
+
+	clothes = { 5, 5, 5, 5, 3 };
+
+	cout << solution(clothes) << "\n";
+
+	clothes = { 10, 10, 1, 1, 10, 10, 10, 7 };
 
 	cout << solution(clothes) << "\n";
 
