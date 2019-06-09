ed7a915ac69bbdab4586e2b8734303712371bc37
warimo47 <warimo47@naver.com>
2019-06-09 12:38:02 +0900

    더 맵게

--- Programmers/더 맵게.cpp
+++ Programmers/더 맵게.cpp
@@ -1,14 +1,39 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K)
 {
 	int answer = 0;
-	bool isNotSpicy = true;
 
+	sort(scoville.begin(), scoville.end());
+
+	while (true)
+	{
+		if (scoville.size() < 2) return -1;
+
+		if (scoville[0] > K && scoville[1] > K) break;
+
+		if (scoville[0] > scoville[2]) scoville[1] = scoville[1] + scoville[2] * 2;
+		else scoville[1] = scoville[0] + scoville[1] * 2;
+		
+		if (scoville.size() > 3)
+		{
+			if (scoville[1] > scoville[3])
+			{
+				scoville[0] = scoville[3];
+				scoville[3] = scoville[1];
+				scoville[1] = scoville[0];
+			}
+		}
+
+		scoville.erase(scoville.begin());
+
+		answer++;
+	}
 
 	return answer;
 }
