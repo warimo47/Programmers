71b1a3244f63fe4d53d19f623412c39231683995
KangsanBae <warimo47@naver.com>
2022-03-02 22:25:11 +0900

    add 네트워크.cpp

---
+++ Programmers/네트워크.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> computers)
+{
+	int answer = 0;
+
+
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution(3, { {1, 1, 0}, {1, 1, 0}, {0, 0, 1} }) << "\n\n";
+
+	cout << solution(3, { {1, 1, 0}, {1, 1, 1}, {0, 1, 1} }) << "\n\n";
+
+	getchar();
+
+	return 0;
+}
--- Programmers/소수 찾기 Level2.cpp
+++ Programmers/소수 찾기 Level2.cpp
@@ -5,19 +5,67 @@
 
 using namespace std;
 
+bool checkPrimenum(int num)
+{
+	if (num < 2) return false;
+
+	for (int i = 2; i * i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void makeNumbers(string nowString, vector<string> leftCards, vector<string>* pSearchNumbers)
+{
+	string saveNowString;
+
+	for (int i = 0; i < leftCards.size(); ++i)
+	{
+		vector<string> copyLeftCards = leftCards;
+
+		saveNowString = nowString + leftCards[i];
+		
+		pSearchNumbers->push_back(saveNowString);
+		copyLeftCards.erase(copyLeftCards.begin() + i);
+
+		makeNumbers(saveNowString, copyLeftCards, pSearchNumbers);
+	}
+}
+
 int solution(string numbers)
 {
-	int answer = 0;
+	vector<string> cards;
+	vector<string> searchNumbers;
 
-	set<int> setNumbers;
+	for (int i = 0; i < numbers.length(); ++i)
+	{
+		cards.push_back(numbers.substr(i, 1));
+	}
 
-	setNumbers.insert(3);
+	makeNumbers("", cards, &searchNumbers);
 
-	return answer;
+	set<int> primeNumbers;
+
+	for (int i = 0; i < searchNumbers.size(); ++i)
+	{
+		if (checkPrimenum(stoi(searchNumbers[i])))
+		{
+			primeNumbers.insert(stoi(searchNumbers[i]));
+		}
+	}
+
+	return primeNumbers.size();
 }
 
 int main()
 {
+	cout << solution("0123456") << "\n\n";
+
 	cout << solution("17") << "\n\n";
 	
 	cout << solution("011") << "\n\n";
